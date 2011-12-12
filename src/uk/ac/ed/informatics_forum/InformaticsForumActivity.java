b94049b80a7eb8eaefbc0fce99f3ac199263ae6d
Cristian Regep <cristian@regep.org>
2011-12-12 22:09:17 +0000

    first commit

---
+++ src/uk/ac/ed/informatics_forum/InformaticsForumActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.informatics_forum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InformaticsForumActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
